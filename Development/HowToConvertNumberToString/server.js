b64b84d1857e33a1f393c4afca6661883bafb596
César Vázquez <agua.ceavaz97@gmail.com>
2025-08-23 16:34:59 -0600

    add new file to Development

---
+++ Development/HowToConvertNumberToString/server.js
@@ -0,0 +1,40 @@
+//HowToConvertNumberToString
+
+/*
+Example 1
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString();
+}
+
+*/
+
+/*
+Example 2
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return String(num);
+}
+
+*/
+
+/*
+Example 3
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return ''+num;
+}
+
+*/
+
+/*
+Example 4
+
+function numberToString(num) {
+  numberToString = num => `${num}`;
+}
+
+*/
